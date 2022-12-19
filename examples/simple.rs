bb09685135d21091a20ab3a673b3c668b8f2d588
Léo Coletta <contact@leo-coletta.fr>
2022-12-19 20:52:49 +0100

    chore: Remove unused deps

--- examples/simple.rs
+++ examples/simple.rs
@@ -9,22 +9,22 @@ struct HelloPayload {
     message: String,
 }
 
-async fn say_hello(_ctx: WorkerContext, payload: HelloPayload) -> anyhow::Result<()> {
+async fn say_hello(_ctx: WorkerContext, payload: HelloPayload) -> Result<(), ()> {
     println!("Hello {} !", payload.message);
     Ok(())
 }
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
+async fn main() {
     std::env::set_var("RUST_LOG", "WARN");
-    tracing_subscriber::fmt::init();
 
-    let pg_options = PgConnectOptions::from_str("postgres://postgres:root@localhost:5432")?;
+    let pg_options = PgConnectOptions::from_str("postgres://postgres:root@localhost:5432").unwrap();
 
     let pg_pool = sqlx::postgres::PgPoolOptions::new()
         .max_connections(5)
         .connect_with(pg_options)
-        .await?;
+        .await
+        .unwrap();
 
     WorkerOptions::default()
         .concurrency(2)
@@ -32,9 +32,9 @@ async fn main() -> anyhow::Result<()> {
         .define_job("say_hello", say_hello)
         .pg_pool(pg_pool)
         .init()
-        .await?
+        .await
+        .unwrap()
         .run()
-        .await?;
-
-    Ok(())
+        .await
+        .unwrap();
 }
